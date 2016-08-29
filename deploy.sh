32f8181af3e1629c278b002295b34243414da13a
Tim Whitacre <tim@timw.co>
2016-08-29 16:01:23 -0400

    last fix - now deploy will work :beer:

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Deploy to GH Pages
-git subtree push --prefix app origin gh-pages
+git subtree push --prefix public origin gh-pages
 
 echo -e "\033[0;32mDone. Thank You, Please Come Again\033[0m"
