6d070b464af6806dacef5cbf4a611b5fc6eecf31
Tim Whitacre <tim@timw.co>
2016-08-29 16:00:48 -0400

    updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,6 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Clean up public directory
-cd public
-find . ! -name '.git' -type f -exec rm -f {} +
-find . ! -name '.git' -type d -exec rm -rf {} +
-cd ..
-
-# Run the basic static file build tool
-gulp
-
-# Build the project.
-hugo
-
 # Deploy to GH Pages
 git subtree push --prefix app origin gh-pages
 
