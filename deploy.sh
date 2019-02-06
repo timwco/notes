ff2c5e21e7ca7227e8cdaec0a8b94005546ccf21
Tim Whitacre <tim@timw.co>
2019-02-06 07:39:41 -0500

    updated deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo "-------------------------------------------------------------"
 
 # Deploy to GH Pages
-git subtree push --prefix public origin gh-pages
+# git subtree push --prefix public origin gh-pages
+git push origin `git subtree split --prefix public`:gh-pages --force
 
-echo -e "\033[0;32mDone. Thank You, Please Come Again\033[0m"
+echo "-------------------------------------------------------------"
+echo "\033[0;32mDone. Thank You, Please Come Again\033[0m"
